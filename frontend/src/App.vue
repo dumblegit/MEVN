634c6a5b50b1810150ed1c20700c93c5b434f600
chema <git.code@protonmail.com>
2022-05-03 15:12:58 +0200

    zip

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app>
-    <v-app-bar
+    <!-- <v-app-bar
       app
       elevation="5"
-      color="indigo darken-2"
+      color="blue-grey"
       dark
     >
       <div class="d-flex align-center">
@@ -36,21 +36,56 @@
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-    </v-app-bar>
+    </v-app-bar> -->
+
+    <v-toolbar dense class="warning">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Tattoo</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+    </v-navigation-drawer>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      drawer: false,
+    };
+  },
 };
 </script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,90 @@
 <template>
-  <hello-world />
+  <v-container white--text>
+    <h1 class="primary--text display-4 mb-3">grid</h1>
+    <v-layout wrap>
+      <v-flex class="primary" xs12>
+        <h1>12</h1>
+      </v-flex>
+      <v-flex class="success" xs6>
+        <h1>6</h1>
+      </v-flex>
+      <v-flex class="error" xs6>
+        <h1>6</h1>
+      </v-flex>
+    </v-layout>
+    <v-layout mt-5 secondary justify-center align-center style="height: 100px">
+      <v-flex class="info" xs3>
+        <h1>centrado</h1>
+      </v-flex>
+      <v-flex class="success" xs3>
+        <h1>centrado</h1>
+      </v-flex>
+    </v-layout>
+    <v-layout warning>
+      <v-flex>
+        <h1 class="display-1">Lorem ipsum</h1>
+        <h1 class="display-2">Lorem ipsum</h1>
+        <h1 class="display-3">Lorem ipsum</h1>
+        <h1 class="display-4">Lorem ipsum</h1>
+      </v-flex>
+    </v-layout>
+
+    <h1 class="primary--text display-4">btn</h1>
+
+    <v-layout>
+      <v-flex>
+        <v-btn class="ml-2">texto</v-btn>
+
+        <v-btn class="success pink--text ml-2">texto</v-btn>
+
+        <v-btn class="info ml-2">texto</v-btn>
+
+        <v-btn class="info ml-2">
+          user
+          <v-icon right>mdi-account-circle</v-icon>
+        </v-btn>
+
+        <v-btn class="ml-2" icon color="pink">
+          <v-icon>mdi-account-circle</v-icon>
+        </v-btn>
+
+        <v-btn class="ml-2" fab color="red">
+          <v-icon dark>mdi-account-outline</v-icon>
+        </v-btn>
+
+        <v-btn class="ml-2" icon color="orange">
+          <v-icon>mdi-thumb-up</v-icon>
+        </v-btn>
+
+        <v-btn class="ml-2" outlined color="error">
+          user
+          <v-icon right>mdi-account-circle</v-icon>
+        </v-btn>
+
+        <v-btn class="ma-2" outlined fab color="teal">
+          <v-icon>mdi-format-list-bulleted-square</v-icon>
+        </v-btn>
+
+        <v-btn class="ml-2" block outlined color="error">
+          user
+          <v-icon right>mdi-account-circle</v-icon>
+        </v-btn>
+
+        <v-btn class="ma-2" block rounded color="error">
+          user
+          <v-icon right>mdi-account-circle</v-icon>
+        </v-btn>
+
+        <div>
+          <v-text-field label="Main input"></v-text-field>
+          <v-text-field label="Another input"></v-text-field>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
+
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {};
 </script>
