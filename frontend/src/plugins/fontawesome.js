7df898c054b6f02c2d4e000ae687785970f31af0
roberto <minifuga@hotmail.com>
2022-05-05 23:25:18 +0200

    Version estable 5/05 sin awepollas

--- frontend/src/plugins/fontawesome.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(fas)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
